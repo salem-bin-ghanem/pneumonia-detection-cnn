d43c6ffb507a7770f5d93d48aaf26c4b3e35e1f5
salem-bin-ghanem <studentschat123@gmail.com>
2026-06-18 10:33:19 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Pneumonia Detection CNN Project")
+
+# This is the main Python file.
+# Later, we will add:
+# 1. Import libraries
+# 2. Load X-ray images
+# 3. Preprocess images
+# 4. Build CNN model
+# 5. Train the model
+# 6. Evaluate results
